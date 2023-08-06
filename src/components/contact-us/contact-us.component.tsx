21466fa9eaf4f7e8642fedc6a5258d896bc68d7a
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2023-08-05 22:38:04 -0400

    Add contact form

--- src/components/contact-us/contact-us.component.tsx
+++ src/components/contact-us/contact-us.component.tsx
@@ -1,52 +1,72 @@
 "use client";
-
-import Image from 'next/image';
-import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 const Contact: React.FC = () => {
-    return (
-        <div className="bg-primary-gray text-white pt-20" id='contact'>
-        <h2 className="text-5xl uppercase font-thin mb-10 text-center">Contact Us</h2>
-        <hr className="border-primary-red border w-36 mb-10 mx-auto" />
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 px-8 py-20">
-          <div className="flex flex-col justify-center mx-auto">
-            <div className="flex items-center mb-8">
-              <Image src="/icons/location.svg" alt="location" width={50} height={50} />
-              <div className="ml-4">
-                <h3 className="text-xl">Address</h3>
-                <p className="text-xl italic leading-7 font-thin">1234 Main St, San Francisco, CA 94122</p>
-              </div>
-            </div>
-            <div className="flex items-center mb-8">
-              <Image src="/icons/call.svg" alt="phone" width={50} height={50} />
-              <div className="ml-4">
-                <h3 className="text-xl">Phone</h3>
-                <p className="text-xl italic leading-7 font-thin">+1 415 555 2671</p>
-              </div>
-            </div>
-            <div className="flex items-center">
-              <Image src="/icons/message.svg" alt="email" width={50} height={50} />
-              <div className="ml-4">
-                <h3 className="text-xl">Email</h3>
-                <p className="text-xl italic leading-7 font-thin">
-                  <a href="mailto:abc@gmail.com" className="text-white hover:underline">abc@gmail.com</a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className='col-span-2'>
-            <iframe
-              title="Map"
-              width="100%"
-              height="400"
-              frameBorder="0"
-              scrolling="no"
-              src={`https://www.openstreetmap.org/export/embed.html?bbox=-81.547136,42.691907,-76.273699,44.521364&layer=mapnik`}
-            ></iframe>
-          </div>
-        </div>
-      </div>
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
 
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <div className="flex justify-center flex-col items-center min-h-screen bg-primary-gray" id='contact'>
+          <h2 className="text-5xl uppercase font-thin mb-10 text-center text-white">Contact Us</h2>
+          <hr className="border-primary-red border w-36" />
+            <form onSubmit={handleSubmit} className="w-1/2 p-6 rounded-md bg-primary-gray text-white">
+                <div className="mb-4">
+                    <label htmlFor="name" className="block text-xl font-semibold mb-2">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        className="w-full p-2 bg-primary-gray_light border border-gray-600 rounded-md text-white"
+                        required
+                    />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-xl font-semibold mb-2">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        className="w-full p-2 bg-primary-gray_light border border-gray-600 rounded-md text-white"
+                        required
+                    />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="message" className="block text-xl font-semibold mb-2">Message</label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleInputChange}
+                        className="w-full p-2 bg-primary-gray_light border border-gray-600 rounded-md resize-none text-white"
+                        rows={4}
+                        required
+                    />
+                </div>
+                <button type="submit" className="bg-primary-red text-white py-2 px-4 rounded-md hover:bg-opacity-80">
+                    Send
+                </button>
+            </form>
+        </div>
     );
 };
 
